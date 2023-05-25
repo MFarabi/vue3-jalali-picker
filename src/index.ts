d7ba6b07cc160204ddaf21c6dd2d785f8607b02d
mansour farabi <mansour.farabi@gmail.com>
2023-05-26 02:12:07 +0330

    add second component

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export { default as DatePicker } from "./components/DatePicker.vue";
+// export { default as DatePicker } from "./components/DatePicker.vue";
+import DatePicker from "./components/DatePicker.vue";
+import InputDatePicker from "./components/InputDatePicker.vue";
+
+export { DatePicker, InputDatePicker };
